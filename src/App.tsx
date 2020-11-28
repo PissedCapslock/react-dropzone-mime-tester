2e0a7162e6e53d16be3d9ee93b0ce9f5c940f741
Robin Stevens <robin.stevens@scz.be>
2020-11-28 09:48:47 +0100

    basic setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import {Grid} from "@material-ui/core"
+
+function App() {
+  return (
+    <Grid container>
+
+
+    </Grid>
+  );
+}
+
+export default App;
